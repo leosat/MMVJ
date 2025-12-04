0607776a64c8291678f4ab470df76098b41c95f7
Leonid Satanovskiy <leonid.satanovsky@gmail.com>
2025-12-04 18:02:35 +0300

    Working draft. Public snapshot.
    Co-authored-by: Leonid Satanovsky <leonid.satanovsky+leosat@gmail.com>

---
+++ src/schemas_predefined.rs
@@ -0,0 +1,55 @@
+#[cfg(feature = "midi")]
+use crate::schemas_midi::MidiMessageCfg;
+use crate::{
+    base_num::BaseNumT, mapped_controls::MappedCtls, num_interval::NumInterval, schemas_cfg::DescriptionCfg,
+    schemas_hid::AxisProperties,
+};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+// use serde_valid::Validate;
+#[derive(JsonSchema, Debug, Clone, Default, Serialize, Deserialize, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub(crate) struct ControlsPredefinedCfg {
+    #[cfg(feature = "midi")]
+    #[serde(default)]
+    pub(crate) midi_controls: BTreeMap<String, MidiControlPredefined>,
+    #[serde(default)]
+    pub(crate) hid_controls: BTreeMap<String, HidControlPredefined>,
+}
+
+#[derive(JsonSchema, Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub(crate) struct HidControlPredefined {
+    pub(crate) r#type: MappedCtls,
+    pub(crate) range: NumInterval<BaseNumT>,
+    #[serde(default)]
+    pub(crate) properties: Option<AxisProperties>,
+    #[serde(default)]
+    pub(crate) initial_value: BaseNumT,
+    #[serde(
+        default,
+        rename = "desc",
+        alias = "description",
+        skip_serializing_if = "DescriptionCfg::is_empty"
+    )]
+    #[schemars(default)]
+    pub(crate) description: DescriptionCfg,
+}
+
+#[cfg(feature = "midi")]
+#[derive(JsonSchema, Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub(crate) struct MidiControlPredefined {
+    pub(crate) midi_message: MidiMessageCfg,
+    pub(crate) range: NumInterval<BaseNumT>,
+    #[serde(
+        default,
+        rename = "desc",
+        alias = "description",
+        skip_serializing_if = "DescriptionCfg::is_empty"
+    )]
+    #[schemars(default)]
+    pub(crate) description: DescriptionCfg,
+}
